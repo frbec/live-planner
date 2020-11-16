75483f08bcb8c2ccf74610d4fbc4fb6863cab2e6
Fredrik Beckius <frbec@Fredrik-Beckius.local>
2020-11-16 16:58:26 +0100

    request user access token

--- functions/fb_login.js
+++ functions/fb_login.js
@@ -1,9 +1,20 @@
-exports.handler = async function(event, context) {
-    // your server-side functionality
-    return {
+exports.handler = async function(event) {
+  import fetch from "node-fetch";
+  // environment variables
+  const { CLIENT_SECRET, CLIENT_ID } = process.env;
+  const API_ENDPOINT = "https://graph.facebook.com/v9.0/oauth/access_token"
+  const REDIRECT_URI = "https://live-planner.netlify.app/.netlify/functions/fb_login"
+
+  // Query the user access token
+  const code = event.queryStringParameters.code
+
+  const request = `${API_ENDPOINT}?code=${code}&redirect_uri=${REDIRECT_URI}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
+  
+  return fetch(request, { headers: { "Accept": "application/json" } })
+    .then(response => response.json())
+    .then(data => ({
       statusCode: 200,
-      body: JSON.stringify({
-        message: "Hello World!"
-      })
-    }
+      body: data.access_token
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
 }
