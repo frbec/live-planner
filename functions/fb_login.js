bdcfdb7445693e004f6fdfe2fe50f414717526b3
Fredrik Beckius <frbec@Fredrik-Beckius.local>
2020-11-16 17:18:24 +0100

    add console...

--- functions/fb_login.js
+++ functions/fb_login.js
@@ -2,16 +2,16 @@ const fetch = require("node-fetch");
 
 // environment variables
 const { CLIENT_SECRET, CLIENT_ID } = process.env;
-const API_ENDPOINT = "https://graph.facebook.com/v9.0/oauth/access_token"
-const REDIRECT_URI = "https://live-planner.netlify.app/.netlify/functions/fb_login"
+const API_ENDPOINT = "https://graph.facebook.com/v9.0/oauth/access_token";
+const REDIRECT_URI = "https://live-planner.netlify.app/.netlify/functions/fb_login";
 
 exports.handler = async function(event) {
 
   // Query the user access token
-  const code = event.queryStringParameters.code
+  const code = event.queryStringParameters.code;
 
   const request = `${API_ENDPOINT}?code=${code}&redirect_uri=${REDIRECT_URI}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
-  
+  console.log(request);
   return fetch(request, { headers: { "Accept": "application/json" } })
     .then(response => response.json())
     .then(data => ({
