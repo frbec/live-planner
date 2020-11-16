c3cde6fc8a36c012707587fa612af24994277b5a
Fredrik Beckius <frbec@Fredrik-Beckius.local>
2020-11-16 16:06:01 +0100

    test server function

---
+++ functions/fb_login.js
@@ -0,0 +1,9 @@
+exports.handler = async function(event, context) {
+    // your server-side functionality
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Hello World!"
+      })
+    }
+}
